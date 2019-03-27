aab21f27fc8ae41495ab05ff198e68398732c8da
randomtable <alefiori3@gmail.com>
2019-03-27 12:54:48 +0100

    Add files via upload

---
+++ build-script.bat
@@ -0,0 +1,23 @@
+DEL /F /Q /A "%~dp0image.txt"
+DEL /F /Q /A "%~dp0container.txt"
+docker system prune -a -f
+docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-Tor-Relay --iidfile "%~dp0image.txt"
+SET /P iid=<"%~dp0image.txt"
+SET iid=%iid:sha256:=%
+docker create %iid%
+docker ps -l -q >> container.txt
+SET /P ciid=<"%~dp0container.txt"
+docker commit %ciid% alessandrofiori/tor-relay:latest
+docker login
+docker push alessandrofiori/tor-relay:latest
+DEL /F /Q /A "%~dp0image.txt"
+DEL /F /Q /A "%~dp0container.txt"
+docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-Tor-World --iidfile "%~dp0image.txt"
+SET /P iid=<"%~dp0image.txt"
+SET iid=%iid:sha256:=%
+docker create %iid%
+docker ps -l -q >> container.txt
+SET /P ciid=<"%~dp0container.txt"
+docker commit %ciid% alessandrofiori/torworld-template:latest
+docker login
+docker push alessandrofiori/torworld-template:latest
