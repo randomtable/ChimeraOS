510dacbf00b1eb88cab161fb3f8987a0a35db44e
randomtable <alefiori3@gmail.com>
2019-03-29 20:19:45 +0100

    Update build-rolling.bat

--- build-rolling.bat
+++ build-rolling.bat
@@ -13,6 +13,19 @@ docker login
 docker push alessandrofiori/tor-relay:latest
 DEL /F /Q /A "%~dp0image.txt"
 DEL /F /Q /A "%~dp0container.txt"
+docker system prune -a -f
+docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-Non-Exit-Relay --iidfile "%~dp0image.txt"
+SET /P iid=<"%~dp0image.txt"
+SET iid=%iid:sha256:=%
+docker create %iid%
+docker ps -l -q >> container.txt
+SET /P ciid=<"%~dp0container.txt"
+docker commit %ciid% alessandrofiori/tor-non-exit-relay:latest
+docker login
+docker push alessandrofiori/tor-non-exit-relay:latest
+DEL /F /Q /A "%~dp0image.txt"
+DEL /F /Q /A "%~dp0container.txt"
+docker system prune -a -f
 docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-Tor-World --iidfile "%~dp0image.txt"
 SET /P iid=<"%~dp0image.txt"
 SET iid=%iid:sha256:=%
