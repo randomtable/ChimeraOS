0187396177fd250faafd9d983119545dc9d3ea05
randomtable <alefiori3@gmail.com>
2019-03-31 13:11:35 +0200

    Update tor-middle-relay-starter.bat

--- tor-middle-relay-starter.bat
+++ tor-middle-relay-starter.bat
@@ -1,3 +1,3 @@
 docker pull alessandrofiori/tor-non-exit-relay
 docker create alessandrofiori/tor-non-exit-relay:latest
-docker run alessandrofiori/tor-non-exit-relay:latest
+docker run -p 9001:9001 alessandrofiori/tor-non-exit-relay:latest
