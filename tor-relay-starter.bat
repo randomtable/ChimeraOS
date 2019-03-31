fb8fd4ccfa0c7d827ae8f7ffc56b9f92ab67d3d9
randomtable <alefiori3@gmail.com>
2019-03-31 13:10:55 +0200

    Update tor-relay-starter.bat

--- tor-relay-starter.bat
+++ tor-relay-starter.bat
@@ -1,3 +1,3 @@
 docker pull alessandrofiori/tor-relay
 docker create alessandrofiori/tor-relay:latest
-docker run alessandrofiori/tor-relay:latest
+docker run -p 9001:9001 alessandrofiori/tor-relay:latest
