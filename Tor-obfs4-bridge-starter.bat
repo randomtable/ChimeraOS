e8ba0f9e59f15061f60c92f6df41bfec5b122857
randomtable <alefiori3@gmail.com>
2019-04-03 22:57:29 +0200

    Update Tor-obfs4-bridge-starter.bat

--- Tor-obfs4-bridge-starter.bat
+++ Tor-obfs4-bridge-starter.bat
@@ -1,3 +1,3 @@
 docker pull alessandrofiori/tor-obfs4-bridge
 docker create alessandrofiori/tor-obfs4-bridge:latest
-docker run -p 9004:9004 alessandrofiori/tor-obfs4-bridge:latest
+docker run -p 9003:9003 alessandrofiori/tor-obfs4-bridge:latest
