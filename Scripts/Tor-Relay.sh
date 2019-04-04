209f42133c0f3898e874586e9791294981dbb1cf
randomtable <alefiori3@gmail.com>
2019-04-04 21:43:45 +0200

    Update Tor-Relay.sh

--- Scripts/Tor-Relay.sh
+++ Scripts/Tor-Relay.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "deb https://deb.torproject.org/torproject.org bionic main" >> /etc/apt/sources.list
 echo "deb-src https://deb.torproject.org/torproject.org bionic main" >> /etc/apt/sources.list
-sudo apt-get install curl -y
 sudo apt-get install gnupg -y
 curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
