d86d1ba68f8a1caafd580ddd3993aee158b17c55
randomtable <alefiori3@gmail.com>
2019-04-04 15:33:08 +0200

    Update build-rolling.bat

--- build-rolling.bat
+++ build-rolling.bat
@@ -35,17 +35,5 @@ SET /P ciid=<"%~dp0container.txt"
 docker commit %ciid% alessandrofiori/torworld-template:latest
 docker login
 docker push alessandrofiori/torworld-template:latest
-DEL /F /Q /A "%~dp0image.txt"
-DEL /F /Q /A "%~dp0container.txt"
-docker system prune -a -f
-docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-Tor-obfs4-bridge --iidfile "%~dp0image.txt"
-SET /P iid=<"%~dp0image.txt"
-SET iid=%iid:sha256:=%
-docker create %iid%
-docker ps -l -q >> container.txt
-SET /P ciid=<"%~dp0container.txt"
-docker commit %ciid% alessandrofiori/tor-obfs4-bridge:latest
-docker login
-docker push alessandrofiori/tor-obfs4-bridge:latest
 TIMEOUT /T 777 /NOBREAK
 GOTO start
