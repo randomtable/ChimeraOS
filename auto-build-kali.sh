a1b16b21769fa480ba82c46ab44a330387240e6c
randomtable <alefiori3@gmail.com>
2019-04-12 12:01:30 +0200

    Update auto-build-kali.sh

--- auto-build-kali.sh
+++ auto-build-kali.sh
@@ -1,4 +1,4 @@
-apt-get install git live-build cdebootstrap
+apt-get install git live-build cdebootstrap -y
 git clone git://git.kali.org/live-build-config.git
 cd live-build-config
 ./build.sh --distribution kali-rolling --verbose
