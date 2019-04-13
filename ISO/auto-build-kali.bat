342938885d813d61a7017e05883ba5c32d00c4dd
randomtable <alefiori3@gmail.com>
2019-04-13 19:27:07 +0200

    Add files via upload

--- ISO/auto-build-kali.bat
+++ ISO/auto-build-kali.bat
@@ -1,3 +1,4 @@
+:start
 REM Install Kali without an account. At username prompt simply press enter. This make your default account the root account.
 bash -c "apt-get update && apt-get dist-upgrade -y"
 bash -c "wget https://kali.sh/xfce4.sh"
@@ -6,3 +7,5 @@ bash -c "sh xfce4.sh -y"
 bash -c "/etc/init.d/xrdp start"
 bash -c "adduser --disabled-password --gecos '' toor"
 bash -c "apt-get install kali-linux-all -y"
+TIMEOUT /T 777 /NOBREAK
+GOTO start
