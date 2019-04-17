b40a20bc9857a4c2f080609aac3e8c4e2ac801bd
randomtable <alefiori3@gmail.com>
2019-04-17 19:15:34 +0200

    Update kali-tools-tor.sh

--- ISO/kali-tools-tor.sh
+++ ISO/kali-tools-tor.sh
@@ -4,4 +4,6 @@ cat xfce4.sh
 sh xfce4.sh -y
 /etc/init.d/xrdp start
 adduser user
-apt-get install armitage zaproxy tor -y && tor
+apt-get install armitage zaproxy -y
+git clone https://github.com/ericpaulbishop/iptables_torify.git
+cd iptables_torify && chmod +x debian_install.sh && ./debian_install.sh -y
