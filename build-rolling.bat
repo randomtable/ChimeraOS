1a733740bd44e7ffa1361d8aa2470bad84015547
randomtable <alefiori3@gmail.com>
2019-08-07 02:52:17 +0200

    Update build-rolling.bat

--- build-rolling.bat
+++ build-rolling.bat
@@ -35,5 +35,17 @@ SET /P ciid=<"%~dp0container.txt"
 docker commit %ciid% alessandrofiori/torworld-template:latest
 docker login
 docker push alessandrofiori/torworld-template:latest
+DEL /F /Q /A "%~dp0image.txt"
+DEL /F /Q /A "%~dp0container.txt"
+docker system prune -a -f
+docker build https://raw.githubusercontent.com/randomtable/ChimeraOS/master/DockerFile-IRC-Server --iidfile "%~dp0image.txt"
+SET /P iid=<"%~dp0image.txt"
+SET iid=%iid:sha256:=%
+docker create %iid%
+docker ps -l -q >> container.txt
+SET /P ciid=<"%~dp0container.txt"
+docker commit %ciid% alessandrofiori/torworld-irc:latest
+docker login
+docker push alessandrofiori/torworld-irc:latest
 TIMEOUT /T 777 /NOBREAK
 GOTO start
